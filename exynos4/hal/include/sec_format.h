ae243ebf935071b77fcc1872e61e9f2319c1d2f1
Greg Hackmann <ghackmann@google.com>
2013-01-03 11:22:37 -0800

    csc: fix custom ARGB_8888 pixel format's name Change-Id: Ie5931a15bcfcfc9c2bdcad416987459d8f85fc2b Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- exynos4/hal/include/sec_format.h
+++ exynos4/hal/include/sec_format.h
@@ -28,7 +28,7 @@ enum {
     HAL_PIXEL_FORMAT_YCbCr_420_SP               = 0x105,
     HAL_PIXEL_FORMAT_YCrCb_422_SP               = 0x106,
     HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED         = 0x107,
-    HAL_PIXEL_FORMAT_ARGB888                    = 0x108,
+    HAL_PIXEL_FORMAT_CUSTOM_ARGB_8888           = 0x108,
     // support custom format for zero copy
     HAL_PIXEL_FORMAT_CUSTOM_YCbCr_420_SP        = 0x110,
     HAL_PIXEL_FORMAT_CUSTOM_YCrCb_420_SP        = 0x111,
