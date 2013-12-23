0dcc416a2c4db4b0f595bfda77899da457a638e6
Dominik Marszk <dmarszk@gmail.com>
2013-12-23 14:02:43 +0100

    macloader: Added another SEMCOSH mac
    Change-Id: Ie86df2ae06e6646bd29362d0cc22fb6fd14aeb67

--- macloader/macloader.cpp
+++ macloader/macloader.cpp
@@ -74,7 +74,8 @@ int main() {
     }
 
     /* semcosh */
-    if (strncasecmp(mac_addr_half, "5c:0a:5b", 9) == 0 ||
+    if (strncasecmp(mac_addr_half, "38:aa:3c", 9) == 0 ||
+        strncasecmp(mac_addr_half, "5c:0a:5b", 9) == 0 ||
         strncasecmp(mac_addr_half, "cc:3a:61", 9) == 0) {
         type = SEMCOSH;
     }
