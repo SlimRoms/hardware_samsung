CyanogenMod/android_hardware_samsung

libril: Merge xmm6262 and xmm6260 to single lib.
The difference between two libs are very minor,
includes one hack around ucsInfo and the logic
to adjust signal strength. Besides that other
diffs are some logs.
The new version of code is minaly based on xmm6262,
especially the logic to adjust singal strength.
Merge them to easy maintain the code.

Change-Id: I76390541d017576591860c8701bb9763c460b8be

ril/libril/Android.mk
@@ -17,7 +17,12 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     librilutils
 
-LOCAL_CFLAGS :=
+ifneq ($(filter xmm6262 xmm6360,$(BOARD_MODEM_TYPE)),)
+LOCAL_CFLAGS := -DMODEM_6262
+endif
+ifeq ($(BOARD_MODEM_TYPE),xmm6260)
+LOCAL_CFLAGS := -DMODEM_6260
+endif
 
 LOCAL_MODULE:= libril
 
@@ -37,7 +42,12 @@ LOCAL_STATIC_LIBRARIES := \
     libcutils \
     librilutils_static
 
-LOCAL_CFLAGS :=
+ifneq ($(filter xmm6262 xmm6360,$(BOARD_MODEM_TYPE)),)
+LOCAL_CFLAGS := -DMODEM_6262
+endif
+ifeq ($(BOARD_MODEM_TYPE),xmm6260)
+LOCAL_CFLAGS := -DMODEM_6260
+endif
 
 LOCAL_MODULE:= libril_static
 

SlimRoms/hardware_samsung

(cherry picked from commit 56b8dcfa3094f93698dda9f6731fa845f00ef287)

ril/libril/Android.mk
@@ -17,10 +17,11 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     librilutils
 
-LOCAL_CFLAGS :=
-
-ifeq ($(BOARD_MODEM_TYPE),xmm7260)
-LOCAL_CFLAGS += -DMODEM_TYPE_XMM7260
+ifneq ($(filter xmm6262 xmm6360,$(BOARD_MODEM_TYPE)),)
+LOCAL_CFLAGS := -DMODEM_TYPE_6262
+endif
+ifeq ($(BOARD_MODEM_TYPE),xmm6260)
+LOCAL_CFLAGS := -DMODEM_TYPE_6260
 endif
 
 LOCAL_MODULE:= libril
@@ -41,7 +42,12 @@ LOCAL_STATIC_LIBRARIES := \
     libcutils \
     librilutils_static
 
-LOCAL_CFLAGS :=
+ifneq ($(filter xmm6262 xmm6360,$(BOARD_MODEM_TYPE)),)
+LOCAL_CFLAGS := -DMODEM_TYPE_6262
+endif
+ifeq ($(BOARD_MODEM_TYPE),xmm6260)
+LOCAL_CFLAGS := -DMODEM_TYPE_6260
+endif
 
 LOCAL_MODULE:= libril_static
 