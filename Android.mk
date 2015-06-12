CyanogenMod/android_hardware_samsung

Samsung: Add common class for vibrator
Change-Id: I0802cc122b606b08f58731febd8bf050d22f4c57

Android.mk
@@ -36,7 +36,7 @@ ifeq ($(BOARD_HAVE_SAMSUNG_WIFI),true)
 include $(SAM_ROOT)/macloader/Android.mk
 endif
 
-# RIL
 ifeq ($(BOARD_VENDOR),samsung)
 include $(SAM_ROOT)/ril/Android.mk
+BOARD_HARDWARE_CLASS := $(SAM_ROOT)/hw
 endif

SlimRoms/hardware_samsung

Android.mk
@@ -36,4 +36,5 @@ endif
 
 ifeq ($(BOARD_VENDOR),samsung)
 include $(SAM_ROOT)/ril/Android.mk
+BOARD_HARDWARE_CLASS := $(SAM_ROOT)/cmhw
 endif