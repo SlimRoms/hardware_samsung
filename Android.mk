d1b83a05c3f51915ce56e5a11bc493544517c51c
Christopher N. Hesse <raymanfx@gmail.com>
2016-01-26 12:17:55 +0100

    Initial unified Samsung lights HAL
    Originally written by Andreas Schneider for Slte.
    
    Change-Id: Iccff1e4532bc460896940451093470ece9086fb9

--- Android.mk
+++ Android.mk
@@ -38,6 +38,7 @@ endif
 ifeq ($(BOARD_VENDOR),samsung)
 BOARD_HARDWARE_CLASS := $(SAM_ROOT)/cmhw
 
+include $(SAM_ROOT)/liblights/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
 include $(SAM_ROOT)/power/Android.mk
 include $(SAM_ROOT)/ril/Android.mk
