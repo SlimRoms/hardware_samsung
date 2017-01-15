CyanogenMod/android_hardware_samsung

power: Update DT2W_PATH
* TARGET_TAP_TO_WAKE_NODE is defined in if condition. But in DT2W_PATH, TARGET_DT2W_PATH is defined. Why?
* Use same flag for both, otherwise we need to define two flags for the dt2w path.
* Use TARGET_TAP_TO_WAKE_NODE instead of TARGET_DT2W_PATH, for similarity with qcom powerHAL.

Change-Id: I87cbdc3f2aa1a3f9f6509580f2c1f001129a0fda

power/Android.mk
@@ -25,7 +25,7 @@ LOCAL_SRC_FILES := power.c
 LOCAL_MODULE_TAGS := optional
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS := -DDT2W_PATH=\"$(TARGET_DT2W_PATH)\"
+    LOCAL_CFLAGS := -DDT2W_PATH=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
 
 include $(BUILD_SHARED_LIBRARY)

SlimRoms/hardware_samsung

power/Android.mk
@@ -25,7 +25,7 @@ LOCAL_SRC_FILES := power.c
 LOCAL_MODULE_TAGS := optional
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS := -DDT2W_PATH=\"$(TARGET_DT2W_PATH)\"
+    LOCAL_CFLAGS := -DTARGET_TAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
 
 include $(BUILD_SHARED_LIBRARY)